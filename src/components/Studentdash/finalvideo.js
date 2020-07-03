741b98c2f9022ca4b564c5f9b11d11e9b6201048
TAppPE <67503643+TAppPE@users.noreply.github.com>
2020-07-03 10:44:20 +0100

    Update finalvideo.js

--- src/components/Studentdash/finalvideo.js
+++ src/components/Studentdash/finalvideo.js
@@ -6,7 +6,7 @@ import React from 'react';
 const finalVid = () => {
     return (
         <div id="finalbtncontainer">
-            <button id="Finalbtn">Submit</button>
+            <button id="Finalbtn"></button>
         </div>
     );
 };
