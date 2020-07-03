6bdeccee23ea74c09241fec2b243888693853ea1
TAppPE <67503643+TAppPE@users.noreply.github.com>
2020-07-03 12:48:09 +0100

    Update avatar.js

--- src/components/Avatar/avatar.js
+++ src/components/Avatar/avatar.js
@@ -3,15 +3,20 @@ import './avatar.css';
 import nav from '../Homepage/navigation';
 import footer from '../Homepage/footer';
 import { Link } from 'react-router-dom';
+import avatarButtons from './avatarButtons';
+import headerAv from './headerAv';
 
 const Nav = nav;
 const Foot = footer;
+const AvBtns = avatarButtons;
+const HeaderAva = headerAv;
 
 const avatar = () => {
     return (
 <div>
-    < Nav />
-    <h1>Create your own avatar</h1>
+     < Nav />
+    < HeaderAva />
+    < AvBtns />
     <Link to = '/choosesports' ><button id="btna">Generate Avatar!</button></Link>
     < Foot />
 </div>
