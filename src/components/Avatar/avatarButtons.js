37a833cf33bf6bc20554110154097ebf6dc60a40
TAppPE <67503643+TAppPE@users.noreply.github.com>
2020-07-03 12:46:59 +0100

    Add files via upload

---
+++ src/components/Avatar/avatarButtons.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './avatarButtons.css';
+
+
+const avatarButtons = () => {
+    return (
+        <div id="avatarBtns">
+            <div id="options">
+               <button class="slidemenu"><span onClick="openNav()">Hair Length</span></button>
+               <button class="slidemenu"><span onClick="openNav()">Face Shape</span></button>
+               <button class="slidemenu"><span onClick="openNav()">Eyes</span></button>
+               <button class="slidemenu"><span onClick="openNav()">Mouth</span></button>
+               <button class="slidemenu"><span onClick="openNav()">Nose</span></button>
+               <button class="slidemenu"><span onClick="openNav()">Clothes</span></button>
+               <button class="slidemenu"><span onClick="openNav()">Other</span>r</button>
+            </div>
+            <div id="avatar">
+                <div id="hair">
+                </div>
+                <div id="face">
+                    <div id="eyes"></div>
+                    <div id="mouth"></div>
+                    <div id="nose"></div>
+                    <div id="eyebrows"></div>
+                    <div id="other"></div>
+                </div>
+                <div id="body"></div>
+
+            </div>
+ </div>
+    )
+};
+
+export default avatarButtons;
---
+++ src/components/Avatar/headerAv.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './headerAv.css';
+
+const headerAv = () => {
+    return (
+       <div id="avHeader">
+           <h1 id="textAvatar">Build your own avatar</h1>
+       </div>
+    )
+};
+
+export default headerAv;
