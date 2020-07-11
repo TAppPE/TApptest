176a8027384ff9c06dc54d24b5b73c596b00d647
TAppPE <67503643+TAppPE@users.noreply.github.com>
2020-07-11 17:09:15 +0100

    Update skills.js

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -11,34 +11,34 @@ const skills = () => {
          <h3 class="number">#1</h3>
          </div>
          <div class="skills">
-         <h3 class="number">#1</h3>
+         <h3 class="number">#2</h3>
          </div>
          <div class="skills">
-         <h3 class="number">#1</h3>
+         <h3 class="number">#3</h3>
          </div>
          <div class="skills">
-         <h3 class="number">#1</h3>
+         <h3 class="number">#4</h3>
          </div>
          <div class="skills">
-         <h3 class="number">#1</h3>
+         <h3 class="number">#5</h3>
          </div>
          <div class="skills">
-         <h3 class="number">#1</h3>
+         <h3 class="number">#6</h3>
          </div>
          <div class="skills">
-         <h3 class="number">#1</h3>
+         <h3 class="number">#7</h3>
          </div>
          <div class="skills">
-         <h3 class="number">#1</h3>
+         <h3 class="number">#8</h3>
          </div>
          <div class="skills">
-         <h3 class="number">#1</h3>
+         <h3 class="number">#9</h3>
          </div>
          <div class="skills">
-         <h3 class="number">#1</h3>
+         <h3 class="number">#10</h3>
          </div>
          <div class="skills">
-         <h3 class="number">#1</h3>
+         <h3 class="number">#11</h3>
          </div>
 
      </div>
