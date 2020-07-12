7f8bbbd2871067389859c071c745fca472c1effb
TAppPE <67503643+TAppPE@users.noreply.github.com>
2020-07-12 20:58:45 +0100

    Add files via upload

---
+++ src/components/Homepage/navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './nav.css';
+
+const nav = () => {
+  return (
+        <div id="nav-background">
+           <div id="nav-buttons">
+             <div id="logo">
+               <button id="tapphp">T<span id="red">A</span><div id="hoverpp"><span id="PP">PP</span></div></button>
+              </div>
+             <div id="endbtns">
+              <button id="ABOUT">ABOUT</button>
+            <button id="LOGIN">LOGIN</button>
+            </div>
+           </div>
+        </div>
+   );
+};
+
+export default nav;
