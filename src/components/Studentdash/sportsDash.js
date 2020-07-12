a706f211915bc4c0e22d7f2be453e4993f9a07f1
TAppPE <67503643+TAppPE@users.noreply.github.com>
2020-07-12 11:43:39 +0100

    Add files via upload

---
+++ src/components/Studentdash/sportsDash.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './sportsDash.css';
+import GlobalFonts from '../font';
+
+const sportsDash = () => {
+    return (
+        <div id="section">
+            < GlobalFonts />
+            <div id="welcomeContainer">
+            <h1 id="welcome"> Hi Sam</h1>
+            <h3 id="welcome2">Welcome to your dashboard</h3>
+            </div>
+           
+            <div id="chosenSportContainer">
+
+                <div class="sportStats">                  
+                   <div id="sportimage">
+                   <div class="hover">
+                    <Link to ='/Footballskill' > <button id="footballBtn"><div class="hover"></div></button> </ Link>
+                </div>
+                       </div>                  
+                   <div id="trafficLight">
+                       <div class="circle"></div>
+                       <div class="circle"></div>
+                       <div class="circle"></div>
+                       <div class="circle"></div>
+                   </div>                 
+                   <div id="videoSubmitted">
+                       <h1 class="recorded">Recorded</h1>
+                       <h2 class="stats">4/4</h2>
+                   </div>
+                   <div id="videoAccepted">
+                       <h1 class="accepted">Accepted</h1>
+                       <h2 class="statsb">4/4</h2>
+                   </div>
+                </div>
+
+                <div class="sportStats">
+                   <div id="sportimageb">
+                       <button class="invisible-btn">
+                       <div class="hover">
+                       <Link to ='/Tennis' > <button id="footballBtn"><div class="hover"></div></button> </ Link>
+                       </div>
+                       </button>
+                       </div>
+                   <div id="trafficLight">
+                         <div class="circle"></div>
+                          <div class="circle"></div>
+                          <div class="circle"></div>
+                          <div class="circle"></div>
+                   </div>
+                   <div id="videoSubmitted">
+                      <h1 class="recorded">Recorded</h1>
+                       <h2 class="stats">4/4</h2>
+                   </div>
+                   <div id="videoAccepted">
+                       <h1 class="accepted">Accepted</h1>
+                       <h2 class="statsb">4/4</h2>
+                   </div>
+                </div>
+
+                <div class="sportStats">
+                   <div id="sportimagec">
+                   <button class="invisible-btn">
+                   <div class="hover">
+                   <h2 class="hiddentext">Football</h2>
+                   </div>
+                   </button>
+                   </div>
+                   <div id="trafficLight">
+                         <div class="circle"></div>
+                          <div class="circle"></div>
+                          <div class="circle"></div>
+                          <div class="circle"></div>
+                   </div>
+                   <div id="videoSubmitted">
+                       <h1 class="recorded">Recorded</h1>
+                       <h2 class="stats">4/4</h2>
+                   </div>
+                   <div id="videoAccepted">
+                      <h1 class="accepted">Accepted</h1>
+                       <h2 class="statsb">4/4</h2>
+                   </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default sportsDash;
