e1713a52c19dc81e8784ae2a7bdbf1669c97cc66
TAppPE <67503643+TAppPE@users.noreply.github.com>
2020-07-14 15:38:59 +0100

    Add files via upload

---
+++ src/components/Teacherdash/addNew.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './addNew.css';
+
+const AddNew = () => {
+    return(
+       <div id="addNewContainer">
+           <div id="makeRows">
+           <button id="addNewBtn">Add New Class +</button>
+           </div>
+       </div>
+    );
+};
+
+export default AddNew;
